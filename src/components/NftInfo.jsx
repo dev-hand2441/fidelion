b00bc21607d0460d851946f1b548d998c490deaa
Seoulman <24202980+dev-hand2441@users.noreply.github.com>
2024-03-07 23:48:24 +0900

    원화 가격 작업

--- src/components/NftInfo.jsx
+++ src/components/NftInfo.jsx
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 function NFTPriceInfo() {
-    const [floorPrice, setFloorPrice] = useState(null);
-    const [listedCount, setListedCount] = useState(null);
+    const [floorPrice, setFloorPrice] = useState(null)
+    const [listedCount, setListedCount] = useState(null)
 
     useEffect(() => {
-        const options = {method: 'GET', headers: {accept: 'application/json'}};
+        const options = { method: 'GET', headers: { accept: 'application/json' } }
 
         fetch('/api/stats', options)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 // API로부터 받은 floorPrice 값을 1,000,000,000로 나누어 상태에 저장
-                const calculatedPrice = (data.floorPrice / 1000000000) * 1.05;
-                setFloorPrice(parseFloat(calculatedPrice.toFixed(2)));
-                setListedCount(data.listedCount);
+                const calculatedPrice = (data.floorPrice / 1000000000) * 1.05
+                setFloorPrice(parseFloat(calculatedPrice.toFixed(2)))
+                setListedCount(data.listedCount)
             })
-            .catch(err => console.error(err));
-    }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 API 호출
+            .catch(err => console.error(err))
+    }, []) // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 API 호출
 
     return (
         <div className="gn-nft-price">
@@ -25,12 +24,14 @@ function NFTPriceInfo() {
                 <h3 className="text-heading">NFT Floor Price</h3>
                 <p>Powered by Magiceden</p>
                 <div className="gn-block">
-                    <b className="text-collection">FIDELION<span>Listed {listedCount}</span></b>
+                    <b className="text-collection">
+                        FIDELION<span>Listed {listedCount}</span>
+                    </b>
                     <p className="text-price">{floorPrice ? `${floorPrice.toFixed(2)} SOL` : 'Loading...'}</p>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default NFTPriceInfo;
+export default NFTPriceInfo
--- src/components/TokenPrice.jsx
+++ src/components/TokenPrice.jsx
@@ -20,7 +20,8 @@ function TokenPrices() {
                 }
             )
             const dataSolana = await responseSolana.json()
-            setPriceSolana(dataSolana.data.value.toFixed(2)) // Solana 토큰 가격 정보 저장 (소수점 2자리)
+            const solanaPrice = dataSolana.data.value.toFixed(2) // Solana 토큰 가격 정보 저장 (소수점 2자리)
+            setPriceSolana(solanaPrice)
 
             // 2080 토큰 가격 정보 요청
             const response2080 = await fetch(
@@ -34,27 +35,28 @@ function TokenPrices() {
                 }
             )
             const data2080 = await response2080.json()
-            setPrice2080(data2080.data.value.toFixed(4)) // 2080 토큰 가격 정보 저장 (소수점 5자리)
+            const token2080Price = data2080.data.value.toFixed(4) // 2080 토큰 가격 정보 저장 (소수점 5자리)
+            setPrice2080(token2080Price)
+
+            // 환율
+            const apiUrl = '/exchange-api?authkey=CKxFJAScH4L3j7YmIpni9PZs14LhBams&searchdate=20240306&data=AP01'
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    // 미국 달러 정보 찾기
+                    const usdInfo = data.find(currency => currency.cur_unit === 'USD')
+                    const kftcDealBasR = parseFloat(usdInfo.kftc_deal_bas_r.replace(/,/g, ''))
+
+                    console.log(usdInfo, kftcDealBasR)
+
+                    // token2080ToKRW 및 solanaToKRW 값 설정
+                    setToken2080ToKRW((token2080Price * kftcDealBasR).toFixed(2))
+                    setSolanaToKRW((solanaPrice * kftcDealBasR).toFixed(2))
+                })
+                .catch(error => console.error('Error fetching data:', error))
         }
 
         fetchPrices() // 토큰 가격 정보 요청 함수 실행
-
-        // 환율
-        const apiUrl = '/exchange-api?authkey=CKxFJAScH4L3j7YmIpni9PZs14LhBams&searchdate=20240306&data=AP01'
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // 미국 달러 정보 찾기
-                const usdInfo = data.find(currency => currency.cur_unit === 'USD')
-                const kftcDealBasR = parseFloat(usdInfo.kftc_deal_bas_r.replace(/,/g, ''))
-
-                console.log(usdInfo, kftcDealBasR, price2080, priceSolana)
-
-                // token2080ToKRW 및 solanaToKRW 값 설정
-                setToken2080ToKRW((price2080 * kftcDealBasR).toFixed(2))
-                setSolanaToKRW((priceSolana * kftcDealBasR).toFixed(2))
-            })
-            .catch(error => console.error('Error fetching data:', error))
     }, [])
 
     return (
