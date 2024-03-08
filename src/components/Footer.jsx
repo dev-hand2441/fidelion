a03fdb69a7af7dfca46083255def28f8684485ab
Seoulman <24202980+dev-hand2441@users.noreply.github.com>
2024-03-08 21:51:20 +0900

    푸터 오픈카톡 비번 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 // Header.jsx
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment } from '@fortawesome/free-solid-svg-icons'
@@ -9,15 +9,25 @@ import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 function Footer() {
     return (
         <footer className="gn-layout-footer">
-            <ul className='gn-layout-footer-channel'>
+            <ul className="gn-layout-footer-channel">
                 <li>
-                    <a className='btn-kakao' href="https://open.kakao.com/o/g10WY6vf" target="_blank">
-                        <b><i className='icon'><FontAwesomeIcon icon={faComment} /></i>카카오톡 오픈 채팅</b>
+                    <a className="btn-kakao" href="https://open.kakao.com/o/g10WY6vf" target="_blank">
+                        <b>
+                            <i className="icon">
+                                <FontAwesomeIcon icon={faComment} />
+                            </i>
+                            카카오톡 오픈 채팅(비번: 191191)
+                        </b>
                     </a>
                 </li>
                 <li>
-                    <a className='btn-discord' href="https://discord.gg/officialfidelion" target="_blank">
-                        <b><i className='icon'><FontAwesomeIcon icon={faDiscord} /></i>공식 디스코드</b>
+                    <a className="btn-discord" href="https://discord.gg/officialfidelion" target="_blank">
+                        <b>
+                            <i className="icon">
+                                <FontAwesomeIcon icon={faDiscord} />
+                            </i>
+                            공식 디스코드
+                        </b>
                     </a>
                 </li>
             </ul>
