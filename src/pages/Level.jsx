c19a6aee1e534baa7fc602eb09d4888c494ce15a
Seoulman <24202980+dev-hand2441@users.noreply.github.com>
2024-03-08 21:48:27 +0900

    줄바꿈 수정

--- src/pages/Level.jsx
+++ src/pages/Level.jsx
@@ -25,27 +25,57 @@ function Level() {
                             <td>-</td>
                         </tr>
                         <tr>
-                            <td>1<i><FontAwesomeIcon icon={faArrowRight} /></i>10</td>
+                            <td>
+                                1
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRight} />
+                                </i>
+                                10
+                            </td>
                             <td>2</td>
                             <td>7,006</td>
                         </tr>
                         <tr>
-                            <td>10<i><FontAwesomeIcon icon={faArrowRight} /></i>20</td>
+                            <td>
+                                10
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRight} />
+                                </i>
+                                20
+                            </td>
                             <td>3</td>
                             <td>8,770</td>
                         </tr>
                         <tr>
-                            <td>20<i><FontAwesomeIcon icon={faArrowRight} /></i>30</td>
+                            <td>
+                                20
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRight} />
+                                </i>
+                                30
+                            </td>
                             <td>4</td>
                             <td>11,970</td>
                         </tr>
                         <tr>
-                            <td>30<i><FontAwesomeIcon icon={faArrowRight} /></i>40</td>
+                            <td>
+                                30
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRight} />
+                                </i>
+                                40
+                            </td>
                             <td>5</td>
                             <td>23,410</td>
                         </tr>
                         <tr>
-                            <td>40<i><FontAwesomeIcon icon={faArrowRight} /></i>50</td>
+                            <td>
+                                40
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRight} />
+                                </i>
+                                50
+                            </td>
                             <td>5</td>
                             <td>47,020</td>
                         </tr>
