77642b1cd1ecd62887e722da52d41e4bb1c45f4a
Seoulman <24202980+dev-hand2441@users.noreply.github.com>
2024-03-11 22:09:26 +0900

    언어 설정 스위치 쿠키로 저장

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,14 @@
 // Header.jsx
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useLanguage } from '../../contexts/LanguageContext'
 
-import { useLanguage } from '../../contexts/LanguageContext';
-
-import Typography from '@mui/material/Typography';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Switch from '@mui/material/Switch'
 
 function Header() {
-    const { language, toggleLanguage } = useLanguage();
+    const { language, toggleLanguage } = useLanguage()
 
     return (
         <header className="gn-layout-header">
@@ -20,15 +18,10 @@ function Header() {
                     <b>Fidelion</b> Calculator
                 </h1>
             </NavLink>
-            <FormGroup className='gn-layout-header-lang'>
+            <FormGroup className="gn-layout-header-lang">
                 <FormControlLabel
-                    control={
-                        <Switch
-                            checked={language === 'en'}
-                            onChange={toggleLanguage}
-                        />
-                    }
-                    label={language === 'en' ? 'EN' : 'KO'}
+                    control={<Switch checked={language === 'en'} onChange={toggleLanguage} />}
+                    label="EN"
                 />
             </FormGroup>
         </header>
--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,21 +1,24 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
 
-const LanguageContext = createContext();
+const LanguageContext = createContext()
 
 export function useLanguage() {
-  return useContext(LanguageContext);
+    return useContext(LanguageContext)
 }
 
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState('ko'); // 기본 언어 설정
+    // 쿠키에서 언어 설정을 가져오거나 기본값을 'ko'로 설정
+    const [language, setLanguage] = useState(Cookies.get('language') || 'ko')
 
-  const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === 'ko' ? 'en' : 'ko'));
-  };
+    useEffect(() => {
+        // 언어 설정이 변경될 때마다 쿠키에 저장
+        Cookies.set('language', language, { expires: 7 }) // 쿠키 유효 기간을 7일로 설정
+    }, [language])
 
-  return (
-    <LanguageContext.Provider value={{ language, toggleLanguage }}>
-      {children}
-    </LanguageContext.Provider>
-  );
-};
+    const toggleLanguage = () => {
+        setLanguage(prevLanguage => (prevLanguage === 'ko' ? 'en' : 'ko'))
+    }
+
+    return <LanguageContext.Provider value={{ language, toggleLanguage }}>{children}</LanguageContext.Provider>
+}
