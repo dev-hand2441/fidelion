be48a89fb1ab5d380034d3f7624fe3dc7a23b183
Seoulman <gn.hand2441@gmail.com>
2024-03-20 15:01:49 +0900

    뉴비 링크 추가

--- src/pages/ko/guide.jsx
+++ src/pages/ko/guide.jsx
@@ -6,6 +6,22 @@ function Guide() {
         <div className="gn-guide">
             <ul>
                 <li className="gn-box">
+                    <a href="https://cafe.naver.com/fidelion?iframe_url=/ArticleList.nhn%3Fsearch.clubid=31167632%26search.menuid=24%26search.boardtype=L" target="_blank">
+                        <b>공지 & 이벤트</b>
+                        <i>
+                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                        </i>
+                    </a>
+                </li>
+                <li className="gn-box">
+                    <a href="https://cafe.naver.com/fidelion/15" target="_blank">
+                        <b>블랙마켓 용어 및 기능</b>
+                        <i>
+                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                        </i>
+                    </a>
+                </li>
+                <li className="gn-box">
                     <a href="https://cafe.naver.com/fidelion/6" target="_blank">
                         <b>팬텀 지갑 생성 & 지갑에 솔라나 입금</b>
                         <i>
@@ -16,6 +32,14 @@ function Guide() {
                 <li className="gn-box">
                     <a href="https://cafe.naver.com/fidelion/17" target="_blank">
                         <b>블랙마켓에서 피델리온 구매하기</b>
+                        <i>
+                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                        </i>
+                    </a>
+                </li>
+                <li className="gn-box">
+                    <a href="https://cafe.naver.com/fidelion/7" target="_blank">
+                        <b>텐서(Tensor)에서 피델리온 구매하기</b>
                         <i>
                             <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                         </i>
